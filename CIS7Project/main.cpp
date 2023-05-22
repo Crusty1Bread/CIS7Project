9d6d2a146425d9b5f2d1c4e2523c1513adf6f2e5
Crusty1Bread <crisvargas3213@gmail.com>
2023-05-21 19:07:02 -0700

    This is a test commit to the Critsian branch

--- CIS7Project/main.cpp
+++ CIS7Project/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
-int main() {
-	//THIS IS A TEST PUSH
+string langMenu();
+string specialMenu();
+
+int main() 
+{
+	/*string language, specialty;
+	* 
+	* 
+	* language = printLangMenu();
+	* 
+	* specialty = printSpecialMenu();
+	* 
+	* 
+	* 
+	* 
+	* 
+	*/
+}
+
+string langMenu()
+{
+	cout << "Language Options:\n--------------\n";
+	cout << "1. English\n2. Spanish\n3. Arabic\n4. French\n5. Italian";
+
+}
+
+string specialMenu()
+{
+
+}
+
+void returnCountry()
+{
+	//Use indexes to generate random numbers to determine the two countries of each language
+	string country[] = { "English", "United Kingdom", "Mexico", "Costa Rica", "Saudi Arabia", "Afghanistan", "France", "Haiti", "Italy", "Sicily" };
+
+	switch () 
+	{
+
+	}
 }
