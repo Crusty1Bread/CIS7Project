820fdc9235adc2f4254376da754959e6a6a654ac
Crusty1Bread <crisvargas3213@gmail.com>
2023-05-21 19:50:07 -0700

    This is an update to your version

--- CIS7Project/main.cpp
+++ CIS7Project/main.cpp
@@ -3,6 +3,52 @@
 
 using namespace std;
 
-int main() {
-	//THIS IS A TEST PUSH
+string langMenu();
+string specialMenu();
+
+int main()
+{
+	string country[] = { "United States", "United Kingdom", "Mexico", "Costa Rica", "Saudi Arabia", "Afghanistan", "France", "Haiti", "Italy", "Sicily" };
+	string languages[] = { "English", "Spanish", "Arabic", "French", "Italian" };
+
+
+	/*string language, specialty;
+	*
+	*
+	* language = printLangMenu();
+	*
+	* specialty = printSpecialMenu();
+	*
+	*
+	* double sampleSpace = country.length() * languages.length();
+	* double prob = 100 * (1/sampleSpace);
+	*
+	* cout << "The chances of this combination is << prob << "%. " << endl;
+	*/
+
+}
+
+string langMenu()
+{
+	cout << "Language Options:\n--------------\n";
+	cout << "1. English\n2. Spanish\n3. Arabic\n4. French\n5. Italian";
+	cout << "--------------\n";
+
+}
+
+string specialMenu()
+{
+	//Print all medical specialties. Request user input for medical specialty.
+	//
+}
+
+void returnCountry(string arr[])
+{
+	//Use indexes to generate random numbers to determine the two countries of each language
+
+
+	switch ()
+	{
+
+	}
 }
